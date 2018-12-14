6f3cdad57218366d8da260c6bc5eee280f1ee78b
Kyle <45437377+cl20myerk@users.noreply.github.com>
2018-12-14 14:25:50 -0500

    Update DiamondsProject.java

--- DiamondsProject.java
+++ DiamondsProject.java
@@ -5,17 +5,18 @@ public class DiamondsProject
 {
 	public static void main(String[] args)throws IOException
 	{
-		File file = new File("C:\\Users\\cl20myerk\\Documents\\GitHub\\Diamond-Project");
+		getInfo();
+	}
+	public static void getInfo()throws IOException
+	{
+		File file = new File("diamonds.csv");
 
 		BufferedReader br = new BufferedReader(new FileReader(file));
-		//Scanner diamonds = new Scanner(new FileReader("C:\\Users\\cl20myerk\\Documents\\GitHub\\Diamond-Project"));
 
 		String st;
 		while((st = br.readLine()) != null)
 		{
 			System.out.println(st);
 		}
-
-		//System.out.println(br);
 	}
 }
