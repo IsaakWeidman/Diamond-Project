69d6181eccad3b73f4fe743c44f86ff039af2ce2
IsaakWeidman <44002541+IsaakWeidman@users.noreply.github.com>
2018-12-17 14:09:53 -0500

    foo

--- DiamondsProject.java
+++ DiamondsProject.java
@@ -3,23 +3,34 @@ import java.io.*;
 public class DiamondsProject {
 	public static void main(String [] args) {
 
-			getData();
+			File inputFile = new File("diamonds.csv");
+			Scanner splitter;
+			List<String> data = new ArrayList<>;
+
+			data = getData(inputFile);
+
+			splitter = new Scanner(data).useDelimiter("\s");
 
 	}// end main()
 
-	public static void getData() {
+	public static List<String> getData(File in) {
+
+		List<String> output = new ArrayList<>();
 
 		try {
 			String line;
 
-			BufferedReader br = new BufferedReader(new FileReader(new File("diamonds.csv")));
+			BufferedReader br = new BufferedReader(new FileReader(in)));
 
 			while((line = br.readLine()) != null)
-				System.out.println(line);
+				output.add(line);
 
 		} catch (IOException e) {
 			System.out.println("File was not found");
 		}
 
+		System.out.println("File input complete");
+
+		return data;
 	}
 }
