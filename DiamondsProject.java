c56d7face25cd689f6e55b27c9664f2e88079fc9
Brennan Mulligan <37249217+brennanmulligan@users.noreply.github.com>
2018-12-17 20:44:17 -0500

    Fixed up DiamondsProject

--- DiamondsProject.java
+++ DiamondsProject.java
@@ -1,30 +1,33 @@
 import java.io.*;
+import java.util.*;
 
 public class DiamondsProject {
 	public static void main(String [] args) {
 
 			File inputFile = new File("diamonds.csv");
 			Scanner splitter;
-			List<String> data = new ArrayList<>;
+			List<String> data = new ArrayList<>();
 			String[][] chart;
 
 			data = getData(inputFile);
 
-			chart = new String[11][chart.size()];
-			splitter = new Scanner(data).useDelimiter("\\,");
+			chart = new String[11][chart.length];
+			//splitter = new Scanner(data).useDelimiter("\\,");
 
 	}// end main()
 
 	public static List<String> getData(File in) {
 
 		List<String> output = new ArrayList<>();
+		Scanner splitter;
 
 		try {
 			String line;
 
-			BufferedReader br = new BufferedReader(new FileReader(in)));
+			BufferedReader br = new BufferedReader(new FileReader(in));
 
 			while((line = br.readLine()) != null)
+				splitter = new Scanner(line).useDelimiter("\\,");
 				output.add(line);
 
 		} catch (IOException e) {
@@ -33,6 +36,6 @@ public class DiamondsProject {
 
 		System.out.println("File input complete");
 
-		return data;
+		return output;
 	}
 }
